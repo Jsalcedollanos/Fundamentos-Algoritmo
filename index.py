ede978aaa9c3f9d8a5bfaeef6ee3a74d011e8859
jsalcedollanos <jhonny_salcedo21@hotmail.com>
2021-08-31 21:09:15 -0500

    Puntos de hayar valor de Y terminados

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ y = float((5 + 2 - 5)**2 * 5 + 8) / (2 - 30) / 2 * 5 - 3
 print('Separamos primero los valores en parentecis ((5 + 2 - 5)^2 * 5 + 8) ')
 print('Luego dividimos el resultado con (2 - 30)')
 print('Por ultimo realice la operacion de: / 2 * 5 - 3')
-print('resultado es: ',y)
+print('resultado del punto 1 es: ',y)
 # FIN DEL PRIMER PUNTO
 
 # PUNTO 2
@@ -24,7 +24,17 @@ y = float((((int(z) + 2 - int(n))^2*int(m))+8/2-30) / 2 * 5  - 3)
 exp = float(pow(y, 5))
 res = float(float(exp)+15*3-9)/3
 
-print('Resultado:',res)
+print('Resultado del punto 2:',res)
+# FIN DEL SEGUNDO PUNTO
+
+# PUNTO 3
+# CALCULAMOS EL VALOR DE Y
+z = 5
+n = float((8 + 2 - 4 ) ** 2 * 5 + 8 +7/2 - 30 * 5) / 2 * 5 - 3 
+m = float(z ** 2 * 3 + n) print('asd')
+y = float((((z + 2 - n ) ** 2 * m + 8 / 2 - 30) / 2 * 5 - 3) ** 5 + 15 * 3 - 9/3) ** 2 - 5/4
+print('Resultado del punto 3 es: ',y)
+# FIN DEL TERCER PUNTO
 
 
 
