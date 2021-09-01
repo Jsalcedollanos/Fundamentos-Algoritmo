0f59ef8ee0f82bdc91f0dd2dc46011aa95de4cc8
jsalcedollanos <jhonny_salcedo21@hotmail.com>
2021-08-31 20:43:35 -0500

    Primer Punto Terminado

--- index.py
+++ index.py
@@ -7,12 +7,11 @@ This is a temporary script file.
 # PUNTO 1
 # CALCULAMOS EL VALOR DE Y
 print("Vamos a calcular el valor de Y")
-
-y = float((5 + 2 - 5)^2 * 5 + 8) / (2 - 30) / 2 * 5 - 3
+y = float((5 + 2 - 5)**2 * 5 + 8) / (2 - 30) / 2 * 5 - 3
 print('Separamos primero los valores en parentecis ((5 + 2 - 5)^2 * 5 + 8) ')
 print('Luego dividimos el resultado con (2 - 30)')
 print('Por ultimo realice la operacion de: / 2 * 5 - 3')
-print('Por ultimo el resultado es: ',y)
+print('resultado es: ',y)
 # FIN DEL PRIMER PUNTO
 
 
